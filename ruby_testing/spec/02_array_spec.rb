9541f76e6eef5a0e50132a34026e4dbeebf057f0
sean-garwood <seansgarwood@gmail.com>
2024-02-29 22:05:15 -0500

    fix: pass 02|update ruby ver
    Didn't want to have two separate commits for the warmup assignment
    copied relevant files from compressed archive
    took me a while to figure out that I was missing some vital config
    files, like rubocop.yml. then, the version set in top's repo (3.2.2)
    was causing some issues with openssl, which i dealt with the other
    day, so i fixed that.
    
    updated to 3.3.0. no issues thus far.

--- ruby_testing/spec/02_array_spec.rb
+++ ruby_testing/spec/02_array_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-describe Array do
+describe Array do # rubocop:disable Metrics/BlockLength
   # An implicitly defined 'subject' is available when the outermost example
   # group is a class. The 'subject' will be an instance of that class.
   # https://rspec.info/features/3-12/rspec-core/subject/implicit-subject/
 
-  # Note: Using an implicit subject is not recommended for most situations.
+  # NOTE: Using an implicit subject is not recommended for most situations.
   # The next lesson will cover explicit subjects, which are recommended over
   # implicit subjects.
 
@@ -15,7 +15,7 @@ describe Array do
     it 'is an Array' do
       expect(subject).to be_an(Array)
     end
-    
+
     # Below is one-line syntax that does the same as the above test.
     # Look at the doc string that is auto-generated when this test is run
     # (in a terminal window).
@@ -24,18 +24,18 @@ describe Array do
 
   context 'when using predicate matchers' do
     context 'when using the empty? predicate method' do
-      # A predicate method in Ruby ends with a ? and only returns true or false. 
+      # A predicate method in Ruby ends with a ? and only returns true or false.
       it 'returns true' do
         expect(subject.empty?).to eq true
       end
     end
-    
+
     # RSpec can leverage this to create predicate matchers for any predicate method.
     # https://rspec.info/features/3-12/rspec-expectations/built-in-matchers/predicates/
     it 'is empty' do
       expect(subject).to be_empty
     end
-    
+
     # Below is one-line syntax that does the same as the above test.
     # Look at the doc string that is auto-generated when this test is run
     # (in a terminal window).
@@ -71,29 +71,24 @@ end
 # ASSIGNMENT
 describe Array do
   context 'when updating an implicit subject' do
-    # remove the 'x' before running this test
-    xit 'is empty' do
-      # Write a test to expect the subject to be empty.
+    it 'is empty' do
+      expect(subject).to be_empty
     end
 
-    # remove the 'x' before running this test
-    xit 'updates length to 1' do
-      # Update the implicit subject to make this test pass.
+    it 'updates length to 1' do
+      subject << 'ssg'
       expect(subject.length).to eq(1)
     end
   end
 
   context 'when using one let variable on two tests' do
-    # Make a let variable that will pass both tests.
+    let(:answer) { [20, 19, 3] }
 
-    # remove the 'x' before running this test
-    xit 'has length of 3' do
-      expect(lucky_numbers.length).to eq(3)
+    it 'has length of 3' do
+      expect(answer.length).to eq(3)
     end
-
-    # remove the 'x' before running this test
-    xit 'has sum of 42' do
-      expect(lucky_numbers.sum).to eq(42)
+    it 'has sum of 42' do
+      expect(answer.sum).to eq(42)
     end
   end
 end
